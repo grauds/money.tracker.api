8bdced6b8ba96970edb81ffdfaffdd0f55d00d60
Anton Troshin <anton.troshin@gmail.com>
2020-10-06 15:00:45 +0300

    Initial commit

---
+++ src/main/java/org/clematis/mt/ClematisMoneyTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.clematis.mt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClematisMoneyTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClematisMoneyTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/clematis/mt/ClematisMoneyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.clematis.mt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClematisMoneyTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
