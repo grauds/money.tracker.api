f8cfc8eb3c57b15678efecb23badc3acc7724438
Anton Troshin <anton.troshin@gmail.com>
2020-10-28 17:58:25 +0300

    [+] Fixed test with mocks

--- src/test/java/org/clematis/mt/ClematisMoneyTrackerApplicationTests.java
+++ src/test/java/org/clematis/mt/ClematisMoneyTrackerApplicationTests.java
@@ -1,13 +1,23 @@
 package org.clematis.mt;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
+@TestConfiguration
 class ClematisMoneyTrackerApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
+    @Bean
+    public DataSource dataSource() {
+        return Mockito.mock(DataSource.class);
+    }
 }
