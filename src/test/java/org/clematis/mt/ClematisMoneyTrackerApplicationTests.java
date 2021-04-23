d1ca0f08dcbaeea7d921ae23a5a89d695c626e88
grauds <antroshin@wiley.com>
2021-04-23 19:07:06 +0300

    [+] Added test containers unit tests

--- src/test/java/org/clematis/mt/ClematisMoneyTrackerApplicationTests.java
+++ src/test/java/org/clematis/mt/ClematisMoneyTrackerApplicationTests.java
@@ -1,23 +1,53 @@
 package org.clematis.mt;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.firebirdsql.testcontainers.FirebirdContainer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
 
-import javax.sql.DataSource;
-
+@Testcontainers
 @SpringBootTest
-@TestConfiguration
 class ClematisMoneyTrackerApplicationTests {
 
-    @Test
-    void contextLoads() {
+    private static final DockerImageName IMAGE =
+            DockerImageName.parse(FirebirdContainer.IMAGE).withTag("2.5.9-sc");
+
+    @Container
+    private static final FirebirdContainer<?> container = new FirebirdContainer<>(IMAGE)
+            .withUsername("testuser")
+            .withPassword("testpassword");
+            //.withCopyFileToContainer(MountableFile.forClasspathResource("mt.fdb"), "/firebird/data");
+
+    @DynamicPropertySource
+    static void init(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.password", container::getPassword);
+        registry.add("spring.datasource.username", container::getUsername);
     }
 
-    @Bean
-    public DataSource dataSource() {
-        return Mockito.mock(DataSource.class);
+    public void canConnectToContainer() throws Exception {
+        try (Connection connection = DriverManager
+                .getConnection(container.getJdbcUrl(), container.getUsername(), container.getPassword());
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery("select CURRENT_USER from RDB$DATABASE")) {
+            Assertions.assertTrue(rs.next(), "has row");
+            Assertions.assertEquals("TESTUSER", rs.getString(1), "user name");
+        }
+    }
+
+    @Test
+    void contextLoads() throws Exception {
+        canConnectToContainer();
     }
 }
