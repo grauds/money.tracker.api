b607dde8d34bec8f0dd28f19e5342571b3d18765
grauds <antroshin@wiley.com>
2022-05-18 20:04:32 +0300

    [+] Added another global CORS configuration

---
+++ src/main/java/org/clematis/mt/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package org.clematis.mt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Ruslan Lagay
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "HEAD", "PUT", "PATCH", "OPTION");
+            }
+        };
+    }
+}
