c88bade695067f12ff3ce2b07bb019a97e62af50
grauds <antroshin@wiley.com>
2022-05-23 16:55:51 +0300

    [+] Fixed checkstyle

--- src/main/java/org/clematis/mt/model/VersionedEntity.java
+++ src/main/java/org/clematis/mt/model/VersionedEntity.java
@@ -1,9 +1,9 @@
 package org.clematis.mt.model;
 
+import javax.persistence.MappedSuperclass;
+
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.MappedSuperclass;
 
 /**
  * Base class for entities with identifier and version
