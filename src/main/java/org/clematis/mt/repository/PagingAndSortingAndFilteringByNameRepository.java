20e465f6d0ba223071e0e2ecf621a315db39a123
grauds <antroshin@wiley.com>
2022-09-14 18:43:25 +0300

    [+] Fixed search by name

--- src/main/java/org/clematis/mt/repository/PagingAndSortingAndFilteringByNameRepository.java
+++ src/main/java/org/clematis/mt/repository/PagingAndSortingAndFilteringByNameRepository.java
@@ -1,11 +1,12 @@
 package org.clematis.mt.repository;
 
-import java.util.List;
-
 import org.clematis.mt.model.NamedEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * This is a trivial extension of the basic Spring repository to add search by the common fields
@@ -17,7 +18,13 @@ import org.springframework.data.repository.query.Param;
 public interface PagingAndSortingAndFilteringByNameRepository<T extends NamedEntity, ID>
         extends PagingAndSortingRepository<T, ID> {
 
-    List<T> findAllByNameContainingIgnoreCase(@Param("name") String name);
+    @RestResource(path = "findByNameStarting")
+    Page<T> findByNameStartingWithIgnoreCase(@Param("name") String name, Pageable pageable);
 
-    List<T> findByNameContains(@Param("name") String name);
+    @RestResource(path = "findByNameContaining")
+    Page<T> findByNameContainingIgnoreCase(@Param("name") String name, Pageable pageable);
+
+    @RestResource(path = "findByNameEnding")
+    Page<T> findByNameEndingWithIgnoreCase(@Param("name") String name, Pageable pageable);
+
 }
