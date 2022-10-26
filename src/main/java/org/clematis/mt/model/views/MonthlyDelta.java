4d3da96aa03d3b645e9d40c43fe810c6b5e971c4
grauds <antroshin@wiley.com>
2022-10-26 11:01:11 +0300

    [+] Wrapped up an embedded key

--- src/main/java/org/clematis/mt/model/views/MonthlyDelta.java
+++ src/main/java/org/clematis/mt/model/views/MonthlyDelta.java
@@ -7,17 +7,28 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
-
 /**
  * @author Anton Troshin
  */
 @Table(name = "MONTHLY_DELTA")
 @Entity
-@Getter
-@Setter
 public class MonthlyDelta implements Serializable {
 
     @EmbeddedId MonthlyDeltaKey key;
+
+    public String getCode() {
+        return this.key.getCode();
+    }
+
+    public int getYear() {
+        return this.key.getAn();
+    }
+
+    public int getMonth() {
+        return this.key.getMois();
+    }
+
+    public double getDelta() {
+        return this.key.getDelta();
+    }
 }
