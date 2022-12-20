be32891ea7855e2431304bc93593959da8f67155
grauds <antroshin@wiley.com>
2022-12-20 19:52:47 +0300

    [+] Fixed monthly delta

--- src/main/java/org/clematis/mt/repository/MonthlyDeltaRepository.java
+++ src/main/java/org/clematis/mt/repository/MonthlyDeltaRepository.java
@@ -2,6 +2,8 @@ package org.clematis.mt.repository;
 
 import org.clematis.mt.model.views.MonthlyDelta;
 import org.clematis.mt.model.views.MonthlyDeltaKey;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -18,7 +20,13 @@ public interface MonthlyDeltaRepository extends PagingAndSortingRepository<Month
             "SELECT ROUND(SUM(MD.DELTA), 2) FROM MONTHLY_DELTA as MD where (MD.AN < :an OR (MD.AN = :an "
                     + "AND MD.MOIS <= :mois)) AND MD.CODE = :code", nativeQuery = true)
     @RestResource(path = "balance")
-    Long getBalanceForCurrency(@RequestParam(value = "an", defaultValue = "0") int an,
-                               @RequestParam(value = "mois", defaultValue = "0") int mois,
+    Long getBalanceForCurrency(@RequestParam(value = "an", defaultValue = "2002") int an,
+                               @RequestParam(value = "mois", defaultValue = "1") int mois,
                                @RequestParam(value = "code", defaultValue = "RUB") String code);
+
+
+    @RestResource(path = "history")
+    @SuppressWarnings("checkstyle:methodname")
+    Page<MonthlyDelta> findMonthlyDeltasByKey_Code(
+            @RequestParam(value = "code", defaultValue = "RUB") String code, Pageable pageable);
 }
