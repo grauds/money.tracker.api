1d07a2625bf51490c8434c175e2078b2926ca4fb
grauds <antroshin@wiley.com>
2022-12-22 11:18:45 +0300

    [+] Added springdoc menu of API groups

--- src/main/java/org/clematis/mt/config/SwaggerConfig.java
+++ src/main/java/org/clematis/mt/config/SwaggerConfig.java
@@ -1,23 +1,20 @@
 package org.clematis.mt.config;
 
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.annotation.PostConstruct;
-
+import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.SwaggerUiConfigProperties;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import lombok.Getter;
+
+import static org.springdoc.core.Constants.ALL_PATTERN;
+
 /**
  *
  * @author Anton Troshin
@@ -45,62 +42,103 @@ public class SwaggerConfig {
 
     private final ApplicationContext applicationContext;
 
+
     public SwaggerConfig(BuildProperties buildProperties, ApplicationContext applicationContext) {
         this.buildProperties = buildProperties;
         this.applicationContext = applicationContext;
     }
 
-    @PostConstruct
-    public void init() {
-        ConfigurableListableBeanFactory beanFactory = ((ConfigurableApplicationContext) applicationContext)
-                .getBeanFactory();
-        AtomicInteger count = new AtomicInteger();
-        dockets().forEach(
-            docket -> beanFactory.registerSingleton("Docket " + count.getAndIncrement(), docket)
-        );
-    }
 
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI();
+        return new OpenAPI().info(getInfo());
     }
 
-    public List<GroupedOpenApi> dockets() {
-        return Stream.of(
-                new GroupedOpenApiInfo("Accounts", new String[] {
-                    "/api/accounts", "/api/accountGroups", "/api/accountsTotals"
-                }),
-                new GroupedOpenApiInfo("Commodities", new String[] {"/api/commodities", "/api/commodityGroups"}),
-                new GroupedOpenApiInfo("Expenses", new String[] {"/api/expenses", "/api/expenseItems"}),
-                new GroupedOpenApiInfo("Income", new String[] {"/api/incomes", "/api/incomeItems"}),
-                new GroupedOpenApiInfo("Money Exchange", new String[] {"/api/moneytypes"}),
-                new GroupedOpenApiInfo("Operations", new String[] {"/api/operations"}),
-                new GroupedOpenApiInfo("Organizations", new String[] {
-                    "/api/organizations", "/api/organizationGroups"
-                })
-            )
-            .map(this::createDocket)
-            .collect(Collectors.toList());
+    public Info getInfo() {
+        return new Info().title("Money Tracker API")
+                .summary("Hateoas Restful API for Dominsoft Money Tracker DB")
+                .description("")
+                .version(buildProperties.getVersion())
+                .description(buildProperties.getName());
     }
 
-    private GroupedOpenApi createDocket(GroupedOpenApiInfo groupedOpenApiInfo) {
-
+    @Bean
+    public GroupedOpenApi accountsApi() {
         return GroupedOpenApi.builder()
-                .group(groupedOpenApiInfo.getName())
-                .pathsToMatch(groupedOpenApiInfo.getFilters())
+                .group("Accounts")
+                .pathsToMatch("/api/accounts", "/api/accountGroups", "/api/accountsTotals")
                 .build();
     }
 
-    @Getter
-    static class GroupedOpenApiInfo {
-
-        private final String name;
-        private final String[] filters;
-
-        GroupedOpenApiInfo(String name, String[] filters) {
-
-            this.name = name;
-            this.filters = filters;
-        }
+    @Bean
+    public GroupedOpenApi commoditiesApi() {
+        return GroupedOpenApi.builder()
+                .group("Commodities")
+                .pathsToMatch("/api/commodities", "/api/commodityGroups")
+                .build();
     }
+
+    @Bean
+    public GroupedOpenApi expensesApi() {
+        return GroupedOpenApi.builder()
+                .group("Expenses")
+                .pathsToMatch("/api/expenses", "/api/expenseItems")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi incomeApi() {
+        return GroupedOpenApi.builder()
+                .group("Income")
+                .pathsToMatch("/api/incomes", "/api/incomeItems")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi moneytypesApi() {
+        return GroupedOpenApi.builder()
+                .group("Money Types")
+                .pathsToMatch("/api/moneytypes")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi operationsApi() {
+        return GroupedOpenApi.builder()
+                .group("Operations")
+                .pathsToMatch("/api/operations")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi organizationsApi() {
+        return GroupedOpenApi.builder()
+                .group("Organizations")
+                .pathsToMatch("/api/organizations", "/api/organizationGroups")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi actuatorApi(OpenApiCustomiser actuatorOpenApiCustomiser,
+                                      WebEndpointProperties endpointProperties) {
+        return GroupedOpenApi.builder()
+                .group("Actuator")
+                .pathsToMatch(endpointProperties.getBasePath() + ALL_PATTERN)
+                .addOpenApiCustomiser(actuatorOpenApiCustomiser)
+                .addOpenApiCustomiser(openApi -> openApi.info(new Info()
+                        .title("Money Tracker Actuator API")
+                        .version(buildProperties.getVersion()))
+                )
+                .pathsToExclude("/rest/actuator/health/**")
+                .pathsToExclude("/rest/actuator/health/*")
+                .build();
+    }
+
+    @Bean
+    SwaggerUiConfigProperties swaggerUiConfig() {
+        SwaggerUiConfigProperties config = new SwaggerUiConfigProperties();
+        config.setShowCommonExtensions(true);
+        return config;
+    }
+
 }
