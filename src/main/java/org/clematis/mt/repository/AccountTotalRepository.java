f0c1e2a23795ab02744d097ebf4a0a25949b7dd3
grauds <antroshin@wiley.com>
2023-01-19 17:43:04 +0300

    [+] Fixed accounts balance in one currency

--- src/main/java/org/clematis/mt/repository/AccountTotalRepository.java
+++ src/main/java/org/clematis/mt/repository/AccountTotalRepository.java
@@ -15,30 +15,55 @@ import org.springframework.data.rest.core.annotation.RestResource;
 @RepositoryRestResource(path = "accountsTotals")
 public interface AccountTotalRepository extends JpaRepository<AccountTotal, Integer> {
 
-    @Query(value = "select atl.ID, atl.NAME, atl.CODE, atl.TOTAL, mt.ID, "
-            + "ROUND(atl.TOTAL / CASE WHEN M.RATE IS NULL THEN 1 ELSE M.RATE END, 2) as BALANCE "
-            + "FROM ACCOUNTS_TOTAL_LAST as atl "
-            + "LEFT JOIN MONEYTYPE as mt on atl.CODE = mt.CODE "
-            + "LEFT JOIN MONEYTYPERATE M on mt.ID = M.SELLMONEYTYPE "
-            + "and M.BUYMONEYTYPE = (SELECT ID FROM MONEYTYPE WHERE CODE=:code) "
-            + "and M.RATEDATE = (SELECT MAX(c.RATEDATE) "
-            + "FROM MONEYTYPERATE as c WHERE c.BUYMONEYTYPE = (SELECT ID FROM MONEYTYPE WHERE CODE=:code) "
-            + "AND c.SELLMONEYTYPE = mt.ID) "
-            + "ORDER BY BALANCE", nativeQuery = true)
+    @Query(value = "select s.AID as ID, s.NAME, s.CODE, s.TOTAL, "
+            + " ROUND(s.TOTAL / (CASE WHEN s.RATE IS NULL THEN "
+            + " (CASE WHEN s.RATE_RUB IS NULL THEN 1 ELSE s.RATE_RUB END) ELSE s.RATE END), 2) as BALANCE from  "
+            + " (SELECT atl.ID as AID, atl.NAME, atl.CODE, atl.TOTAL, mt.ID, "
+            + "       (SELECT first 1 * from (SELECT ROUND(mtr1.RATE/mtr2.RATE, 15) as RATE  "
+            + "                        FROM MONEYTYPERATE as mtr1  "
+            + "                        LEFT JOIN MONEYTYPERATE as mtr2  "
+            + "                         ON mtr1.RATEDATE = mtr2.RATEDATE  "
+            + "                             AND mtr1.SELLMONEYTYPE != 1  "
+            + "                             AND mtr2.SELLMONEYTYPE != 1  "
+            + "                             AND mtr1.SELLMONEYTYPE = mt.ID  "
+            + "                             AND mtr2.SELLMONEYTYPE = (SELECT ID FROM MONEYTYPE WHERE CODE=:code)  "
+            + "                        WHERE (mtr1.RATE IS NOT NULL AND mtr2.RATE IS NOT NULL) "
+            + " ORDER BY mtr1.RATEDATE DESC)  "
+            + "                    ) as RATE,  "
+            + "       (SELECT first 1 * from (select ROUND(mtr.RATE, 15) as RATE  "
+            + "                       FROM MONEYTYPERATE as mtr  "
+            + "                       WHERE (mtr.BUYMONEYTYPE = (SELECT ID FROM MONEYTYPE WHERE CODE=:code) "
+            + " AND mtr.SELLMONEYTYPE = mt.ID)  "
+            + "                       ORDER BY mtr.RATEDATE DESC)  "
+            + "                    ) as RATE_RUB  "
+            + "    FROM ACCOUNTS_TOTAL_LAST as atl  "
+            + "    LEFT JOIN MONEYTYPE as mt on atl.CODE = mt.CODE) as s", nativeQuery = true)
     @RestResource(path = "code")
     List<AccountTotal> getAccountTotals(@Param(value = "code") String code);
 
 
-    @Query(value = "select ROUND(sum(atl.TOTAL / CASE WHEN M.RATE IS NULL THEN 1 ELSE M.RATE END), 2) "
-            + "FROM ACCOUNTS_TOTAL_LAST as atl "
-            + "    LEFT JOIN MONEYTYPE as mt on atl.CODE = mt.CODE "
-            + "    LEFT JOIN MONEYTYPERATE M on mt.ID = M.SELLMONEYTYPE "
-            + "                             and M.BUYMONEYTYPE = (SELECT ID FROM MONEYTYPE WHERE "
-            + "ID=(SELECT ID FROM MONEYTYPE WHERE CODE=:code)) "
-            + "                             and M.RATEDATE = (SELECT MAX(c.RATEDATE) FROM MONEYTYPERATE as c "
-            + "                                     WHERE c.BUYMONEYTYPE = (SELECT ID FROM MONEYTYPE WHERE "
-            + "ID=(SELECT ID FROM MONEYTYPE WHERE CODE=:code)) "
-            + "                                     AND c.SELLMONEYTYPE = mt.ID)", nativeQuery = true)
+    @Query(value = "select ROUND(sum(s.TOTAL / (CASE WHEN s.RATE IS NULL THEN"
+            + " (1/(CASE WHEN s.RATE_RUB IS NULL THEN 1 ELSE s.RATE_RUB END)) ELSE s.RATE END)), 2) as BALANCE from  "
+            + "(SELECT atl.ID as AID, atl.NAME, atl.CODE, atl.TOTAL, mt.ID,  "
+            + "       (SELECT first 1 * from (SELECT ROUND(mtr1.RATE/mtr2.RATE, 15) as RATE  "
+            + "                        FROM MONEYTYPERATE as mtr1  "
+            + "                        LEFT JOIN MONEYTYPERATE as mtr2  "
+            + "                         ON mtr1.RATEDATE = mtr2.RATEDATE  "
+            + "                             AND mtr1.SELLMONEYTYPE != 1  "
+            + "                             AND mtr2.SELLMONEYTYPE != 1  "
+            + "                             AND mtr1.SELLMONEYTYPE = mt.ID  "
+            + "                             AND mtr2.SELLMONEYTYPE = (SELECT ID FROM MONEYTYPE WHERE CODE=:code)  "
+            + "                        WHERE (mtr1.RATE IS NOT NULL AND mtr2.RATE IS NOT NULL) "
+            + " ORDER BY mtr1.RATEDATE DESC)  "
+            + "                    ) as RATE,  "
+            + "       (SELECT first 1 * from (select ROUND(mtr.RATE, 15) as RATE  "
+            + "                       FROM MONEYTYPERATE as mtr  "
+            + "                       WHERE (mtr.BUYMONEYTYPE = mt.ID AND mtr.SELLMONEYTYPE = "
+            + " (SELECT ID FROM MONEYTYPE WHERE CODE=:code))  "
+            + "                       ORDER BY mtr.RATEDATE DESC)  "
+            + "                    ) as RATE_RUB  "
+            + "    FROM ACCOUNTS_TOTAL_LAST as atl  "
+            + "    LEFT JOIN MONEYTYPE as mt on atl.CODE = mt.CODE) as s", nativeQuery = true)
     @RestResource(path = "balance")
     Long getBalanceForCurrency(@Param(value = "code") String code);
 
