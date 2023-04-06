1c4819543e8c048b511db5fb91d100f8fe6b09c6
grauds <antroshin@wiley.com>
2023-04-06 19:03:37 +0300

    [*] Migrated schema init param for Spring Boot

--- src/main/java/org/clematis/mt/repository/ExpenseItemRepository.java
+++ src/main/java/org/clematis/mt/repository/ExpenseItemRepository.java
@@ -6,8 +6,8 @@ import org.clematis.mt.model.ExpenseItem;
 import org.clematis.mt.model.ExpenseItemEntry;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author Anton Troshin
  */
 @RepositoryRestResource(path = "expenseItems", excerptProjection = ExpenseItemEntry.class)
-public interface ExpenseItemRepository extends PagingAndSortingRepository<ExpenseItem, Integer> {
+public interface ExpenseItemRepository extends JpaRepository<ExpenseItem, Integer> {
 
     @RestResource(path = "commodity")
     Page<ExpenseItem> findByCommodityId(@Param(value = "commodityId") int commodityId, Pageable pageable);
@@ -27,7 +27,7 @@ public interface ExpenseItemRepository extends PagingAndSortingRepository<Expens
     Page<ExpenseItem> findByTradeplaceId(@Param(value = "tradeplaceId") int tradeplaceId, Pageable pageable);
 
     @RestResource(path = "filtered")
-    Page<ExpenseItem> findByTransferdateGreaterThanAndTransferdateLessThan(
+    Page<ExpenseItem> findByTransferdateGreaterThanEqualAndTransferdateLessThanEqual(
             @RequestParam(value = "startDate", required = false)
             @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate,
             @RequestParam(value = "endDate", required = false)
--- src/main/java/org/clematis/mt/repository/PagingAndSortingAndFilteringByNameRepository.java
+++ src/main/java/org/clematis/mt/repository/PagingAndSortingAndFilteringByNameRepository.java
@@ -3,8 +3,8 @@ package org.clematis.mt.repository;
 import org.clematis.mt.model.NamedEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -17,7 +17,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
  */
 @NoRepositoryBean
 public interface PagingAndSortingAndFilteringByNameRepository<T extends NamedEntity, ID>
-        extends PagingAndSortingRepository<T, ID> {
+        extends JpaRepository<T, ID> {
 
     @RestResource(path = "findByNameStarting")
     Page<T> findByNameStartingWithIgnoreCase(@Param("name") String name, Pageable pageable);
