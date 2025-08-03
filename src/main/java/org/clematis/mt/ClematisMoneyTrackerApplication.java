2f4872489bac3068e3541dd07711e99dba7d2533
Anton Troshin <11165918+grauds@users.noreply.github.com>
2025-08-03 16:24:54 +0300

    [+] Added logging for headers

--- src/main/java/org/clematis/mt/ClematisMoneyTrackerApplication.java
+++ src/main/java/org/clematis/mt/ClematisMoneyTrackerApplication.java
@@ -1,5 +1,14 @@
 package org.clematis.mt;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +16,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.stereotype.Component;
 
+import lombok.extern.java.Log;
 /**
  * @author Anton Troshin
  */
@@ -18,7 +29,8 @@ public class ClematisMoneyTrackerApplication {
 
     @Bean
     public GrantedAuthorityDefaults grantedAuthorityDefaults() {
-        // Remove the default ROLE_ prefix that spring boot otherwise expects
+
+        // Remove the default ROLE_ prefix that Spring Boot otherwise expects
         return new GrantedAuthorityDefaults("");
     }
 
@@ -29,6 +41,18 @@ public class ClematisMoneyTrackerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClematisMoneyTrackerApplication.class, args);
+    }
+
+    @Log
+    @Component
+    public class HeaderLoggerFilter implements Filter {
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+            HttpServletRequest req = (HttpServletRequest) request;
+            log.info("X-Forwarded-Proto: " + req.getHeader("X-Forwarded-Proto"));
+            log.info("X-Forwarded-Host: " + req.getHeader("X-Forwarded-Host"));
+            chain.doFilter(request, response);
+        }
     }
 
 
