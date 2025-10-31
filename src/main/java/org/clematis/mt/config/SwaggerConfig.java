99d4b19b30bff3274f64637afa56c6c135ee124f
Anton Troshin <11165918+grauds@users.noreply.github.com>
2025-10-31 18:20:28 +0300

    [+] Updated to the latest deps versions

--- src/main/java/org/clematis/mt/config/SwaggerConfig.java
+++ src/main/java/org/clematis/mt/config/SwaggerConfig.java
@@ -2,11 +2,10 @@ package org.clematis.mt.config;
 
 import static org.springdoc.core.Constants.ALL_PATTERN;
 import org.springdoc.core.GroupedOpenApi;
-import org.springdoc.core.customizers.OpenApiCustomiser;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
@@ -19,6 +18,9 @@ import io.swagger.v3.oas.models.info.Info;
  * @author Anton Troshin
  */
 @Configuration
+@ComponentScan(
+    basePackages = {"org.clematis.mt"}
+)
 @SecurityScheme(
     name = "Bearer Authentication",
     type = SecuritySchemeType.HTTP,
@@ -39,92 +41,127 @@ public class SwaggerConfig {
     }
 
     public Info getInfo() {
-        return new Info().title("Money Tracker API")
-                .summary("Hateoas Restful API for Dominsoft Money Tracker DB")
-                .description("")
-                .version(buildProperties.getVersion())
-                .description(buildProperties.getName());
+        return new Info().title("Clematis Money Tracker API")
+            .summary("Hateoas Restful API for Dominsoft Money Tracker DB")
+            .description(buildProperties.getName())
+            .version(buildProperties.getVersion());
     }
 
     @Bean
     public GroupedOpenApi accountsApi() {
         return GroupedOpenApi.builder()
             .group("Accounts")
-            .pathsToMatch("/api/accounts", "/api/accountGroups", "/api/accountsTotals")
+            .pathsToMatch("/api/accounts/**", "/api/accountGroups/**", "/api/accountsTotals/**")
+            .addOpenApiCustomiser(openApi -> openApi.info(
+                new Info()
+                    .title("Accounts API")
+                    .version(buildProperties.getVersion())
+            ))
             .build();
     }
 
     @Bean
     public GroupedOpenApi commoditiesApi() {
         return GroupedOpenApi.builder()
-                .group("Commodities")
-                .pathsToMatch("/api/commodities", "/api/commodityGroups")
-                .build();
+            .group("Commodities")
+            .pathsToMatch("/api/commodities/**", "/api/commodityGroups/**")
+            .addOpenApiCustomiser(openApi -> openApi.info(
+                new Info()
+                    .title("Commodities API")
+                    .version(buildProperties.getVersion())
+            ))
+            .build();
     }
 
     @Bean
     public GroupedOpenApi expensesApi() {
         return GroupedOpenApi.builder()
-                .group("Expenses")
-                .pathsToMatch("/api/expenses", "/api/expenseItems", "/api/agentCommodityGroupExpenses")
-                .build();
+            .group("Expenses")
+            .pathsToMatch("/api/expenses/**", "/api/expenseItems/**", "/api/agentCommodityGroupExpenses/**")
+            .addOpenApiCustomiser(openApi -> openApi.info(
+                new Info()
+                    .title("Expenses API")
+                    .version(buildProperties.getVersion())
+            ))
+            .build();
     }
 
     @Bean
     public GroupedOpenApi incomeApi() {
         return GroupedOpenApi.builder()
-                .group("Income")
-                .pathsToMatch("/api/incomes", "/api/incomeItems")
-                .build();
+            .group("Income")
+            .pathsToMatch("/api/incomes/**", "/api/incomeItems/**")
+            .addOpenApiCustomiser(openApi -> openApi.info(
+                new Info()
+                    .title("Income API")
+                    .version(buildProperties.getVersion())
+            ))
+            .build();
     }
 
     @Bean
     public GroupedOpenApi moneytypesApi() {
         return GroupedOpenApi.builder()
-                .group("Money Types")
-                .pathsToMatch("/api/moneytypes")
-                .build();
+            .group("Money Types")
+            .pathsToMatch("/api/moneytypes/**")
+            .addOpenApiCustomiser(openApi -> openApi.info(
+                new Info()
+                    .title("Money Types API")
+                    .version(buildProperties.getVersion())
+            ))
+            .build();
     }
 
     @Bean
     public GroupedOpenApi operationsApi() {
         return GroupedOpenApi.builder()
-                .group("Operations")
-                .pathsToMatch("/api/operations")
-                .build();
+            .group("Operations")
+            .pathsToMatch("/api/operations/**")
+            .addOpenApiCustomiser(openApi -> openApi.info(
+                new Info()
+                    .title("Operations API")
+                    .version(buildProperties.getVersion())
+            ))
+            .build();
     }
 
     @Bean
     public GroupedOpenApi exchangeApi() {
         return GroupedOpenApi.builder()
-                .group("Money Exchange")
-                .pathsToMatch("/api/exchange")
-                .build();
+            .group("Money Exchange")
+            .pathsToMatch("/api/exchange/**")
+            .addOpenApiCustomiser(openApi -> openApi.info(
+                new Info()
+                    .title("Money Exchange API")
+                    .version(buildProperties.getVersion())
+            ))
+            .build();
     }
 
     @Bean
     public GroupedOpenApi organizationsApi() {
         return GroupedOpenApi.builder()
-                .group("Organizations")
-                .pathsToMatch("/api/organizations", "/api/organizationGroups")
-                .build();
+            .group("Organizations")
+            .pathsToMatch("/api/organizations/**", "/api/organizationGroups/**")
+            .addOpenApiCustomiser(openApi -> openApi.info(
+                new Info()
+                    .title("Organizations API")
+                    .version(buildProperties.getVersion())
+            ))
+            .build();
     }
 
     @Bean
-    public GroupedOpenApi actuatorApi(@Qualifier("actuatorOpenApiCustomizer")
-                                      OpenApiCustomiser actuatorOpenApiCustomiser,
-                                      WebEndpointProperties endpointProperties) {
+    public GroupedOpenApi actuatorApi(WebEndpointProperties endpointProperties) {
         return GroupedOpenApi.builder()
-                .group("Actuator")
-                .pathsToMatch(endpointProperties.getBasePath() + ALL_PATTERN)
-                .addOpenApiCustomiser(actuatorOpenApiCustomiser)
-                .addOpenApiCustomiser(openApi -> openApi.info(
+            .group("Actuator")
+            .pathsToMatch(endpointProperties.getBasePath() + ALL_PATTERN)
+            .addOpenApiCustomiser(openApi -> openApi.info(
                     new Info()
-                        .title("Money Tracker Actuator API")
+                        .title("MT Actuator API")
                         .version(buildProperties.getVersion())
-                    )
                 )
-                .build();
+            )
+            .build();
     }
-
 }
