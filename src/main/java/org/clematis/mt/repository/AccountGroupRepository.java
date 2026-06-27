3dd25499ffbe8794825c83f7573966d44b5c9a45
Anton Troshin <11165918+grauds@users.noreply.github.com>
2026-06-27 20:50:29 +0300

    [+] Recursive by parent id for accounts

--- src/main/java/org/clematis/mt/repository/AccountGroupRepository.java
+++ src/main/java/org/clematis/mt/repository/AccountGroupRepository.java
@@ -1,12 +1,41 @@
 package org.clematis.mt.repository;
 
 import org.clematis.mt.model.AccountGroup;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * @author Anton Troshin
  */
 @RepositoryRestResource(path = "accountGroups")
 public interface AccountGroupRepository extends PagingAndSortingAndFilteringByNameRepository<AccountGroup, Integer> {
-
+    @Query(
+        value = """
+            SELECT * FROM (WITH RECURSIVE w1(id, parent, name) AS
+            (SELECT c.id, c.parent, c.name
+                FROM ACCOUNTGROUP as c
+                WHERE c.id = :id
+            UNION ALL
+            SELECT c2.id, c2.parent, c2.name
+            FROM w1 JOIN ACCOUNTGROUP as c2 ON c2.parent=w1.id
+            )
+            SELECT * FROM w1 WHERE w1.id <> :id ORDER BY name)""",
+        countQuery = """
+            SELECT COUNT(*) FROM (WITH RECURSIVE w1(id, parent, name) AS
+            (SELECT c.id, c.parent, c.name
+                FROM ACCOUNTGROUP as c
+                WHERE c.id = :id
+            UNION ALL
+            SELECT c2.id, c2.parent, c2.name
+            FROM w1 JOIN ACCOUNTGROUP as c2 ON c2.parent=w1.id
+            )
+            SELECT * FROM w1 WHERE w1.id <> :id ORDER BY name)""",
+        nativeQuery = true
+    )
+    @RestResource(path = "recursiveByParentId")
+    Page<AccountGroup> findRecursiveByParentId(@Param("id") Integer id, Pageable pageable);
 }
